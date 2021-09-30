d03f82d89ddecb4657d968f6b9cd9f1f31baf2cb
Joe Feghaly <joe.feghaly@me.com>
2021-09-30 06:41:42 +0300

    Final Chapter 4

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -17,10 +17,16 @@ export default class Counter extends Component {
     render() {
         return (
             <>
-                <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm" onClick={()=>{this.props.onIncrement(this.props.counter.id)}}>+</button>
-                <button className="btn btn-secondary btn-sm m-2" onClick={()=>{this.props.onDecrement(this.props.counter.id)}}>-</button>
-                <button className="btn btn-danger btn-sm m-2" onClick={()=>{this.props.onDelete(this.props.counter.id)}}>delete</button>
+            <div className="row">
+                <div className="col-1">
+                    <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                </div>
+                <div className="col">
+                    <button className="btn btn-secondary btn-sm" onClick={()=>{this.props.onIncrement(this.props.counter.id)}}>+</button>
+                    <button className="btn btn-secondary btn-sm m-2" onClick={()=>{this.props.onDecrement(this.props.counter.id)}}>-</button>
+                    <button className="btn btn-danger btn-sm m-2" onClick={()=>{this.props.onDelete(this.props.counter.id)}}>delete</button>
+                </div>
+            </div>
             </>
         )
     }
